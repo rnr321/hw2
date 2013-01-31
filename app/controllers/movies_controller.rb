4651d5e588468e696b164f41b07ddbd403a31beb
saasbook <saasbook@saasbook.(none)>
2013-01-31 00:23:15 -0800

    commit for deploy to heroku

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-
+helper_method :sort_column, :sort_direction
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -7,12 +7,29 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    #@movies = Movie.all
+    @movies = Movie.order (sort_column + " " + sort_direction)	
+    @all_ratings = Movie.all_ratings
+    @selected_ratings = (params[:ratings].present? ? params[:ratings] : [])
   end
 
   def new
     # default: render 'new' template
   end
+
+private
+
+  def filter_selection	
+   params[:ratings] == nil ? Movie.all_ratings : params[:ratings]	
+  end
+	
+ def sort_column
+	 Movie.column_names.include?(params[:sort]) ? params[:sort] : "id"
+ end
+
+def sort_direction
+%w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+end
 
   def create
     @movie = Movie.create!(params[:movie])
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,14 @@
 class Movie < ActiveRecord::Base
+	
+  def self.all_ratings
+ #self.select (:rating).map (&:rating).uniq
+end
+	
+  def self.filter (selection)
+#begin
+#  self.where (:rating => selection.keys)
+#rescue
+#  self.where (:rating => selection)
+#end
+ end
 end
